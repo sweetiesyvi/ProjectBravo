eeef2654d432d437fd324ad8a304c3baae1e0b0f
sweetiesyvi <rineemwky@gmail.com>
2025-10-14 22:06:54 -0700

    Update storage.js

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,8 +1,11 @@
 export function savePlayerName(name) {
   localStorage.setItem('playerName', name);
+  playerNameInput.value = loadPlayerName();
+
 }
 
 export function loadPlayerName() {
   return localStorage.getItem('playerName') || '';
 }
+
 
